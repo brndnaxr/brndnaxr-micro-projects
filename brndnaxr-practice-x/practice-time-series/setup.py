14afb13b712e07e2269c06a524087fa5c5fb98c1
brndnaxr <48624376+brndnaxr@users.noreply.github.com>
2021-08-25 15:07:58 -0300

    added notebooks of time series

---
+++ brndnaxr-practice-x/practice-time-series/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Coming soon, i.e. the description',
+    author='Brandon Aguiar',
+    license='MIT',
+)
